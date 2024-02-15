f62e33b8cc61db605fde57e03afd0195b3b40801
Aisha SEISENBAYEVA <aseisenb@bess-f3r1s14.clusters.42paris.fr>
2024-02-15 17:12:22 +0100

    main

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,11 +1,7 @@
 #include "Struct.hpp"
 #include "User.hpp"
 
-/*
-	TODO
-	initialize fds of data to -1, to check in clear_data if the fd has to be closed or not
-*/
-
+// SIGINT signal (Ctrl+C)
 void	signal_handler(int code)
 {
 	(void)code;
@@ -18,24 +14,32 @@ int main(int argc, char *argv[])
 	int					epoll_fds;
 	struct sigaction	sa;
 
+	// 1. Command line arguments check
 	if (argc != 3)
-		return (error("ircserver requires 2 arguments. Usage: ./ircserver <PORT> <PASSWORD>"), EXIT_FAILURE);
+		return (error("Usage: ./ircserver <port> <password>"), EXIT_FAILURE);
+	// 2. Parsing command line arguments
 	if (!parsing(argv, data.port, data.password))
 		return (EXIT_FAILURE);
+	// 3. Initializing the server
 	if (!initialize(data.port, data))
 		return (EXIT_FAILURE);
+	// 4. Setting up signal handling
 	g_data_ptr = &data;
 	sa.sa_handler = signal_handler;
-	sa.sa_flags = SA_RESTART; 					//Set to 0 if causes problems with syscalls
-	sigfillset(&sa.sa_mask);					//Blocks the other signals when the handler is called
+	sa.sa_flags = SA_RESTART; 		
+	sigfillset(&sa.sa_mask);
 	if (sigaction(SIGINT, &sa, NULL) < 0)
-		clear_exit_data(data, "sigaction() error", 1);
-	cout << "Port: " << data.port << endl << "Password: " << data.password << endl << "Enjoy ;)" << endl;
+		clear_exit_data(data, "Sigaction error", 1);
+	// 5. Print server info
+	cout << "Port: " << data.port << endl << "Password: " << data.password << endl << "I am ready" << endl;
+	// 6 Main server loop
 	while (true)
 	{
+		// 7. Waiting for events using epoll
 		epoll_fds = epoll_wait(data.epoll.fd, data.epoll.events, MAX_CNCTS, -1);
 		if (epoll_fds < 0)
-			return (clear_data(data), error("epoll_wait() failed"), EXIT_FAILURE);
+			return (clear_data(data), error("epoll_wait function error"), EXIT_FAILURE);
+		// 8. Handling events in a loop
 		for (int i = 0; i < epoll_fds; i++)
 			central_server(data, i);		
 	}
